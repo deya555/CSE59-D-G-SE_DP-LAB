0941b7da10196dcba98d703e392f4e8e10e9c130
deya555 <167282964+deya555@users.noreply.github.com>
2026-06-18 16:57:04 +0600

    Add Countdown class to display countdown from 5

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,14 @@
-
+class Countdown {
+    void start (){
+        for(int i=5;i>=1;i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main {
+    public static void main (String[] args){
+        Countdown count = new Countdown();
+        count.start();
+    }
+}
