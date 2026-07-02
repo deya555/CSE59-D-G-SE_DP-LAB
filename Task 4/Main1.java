5da26ee67087c2eec02c852c208b8717e6ef64da
deya555 <167282964+deya555@users.noreply.github.com>
2026-07-02 17:26:46 +0600

    Correct Java syntax and class structure
    Fix import statements, class names, and variable declarations.

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,39 @@
+import java.util.scanner
+public class main41{
+    public static void main(String[] args){
+        scanner sc = new scanner(System.in);
+        System.out.println("name");
+        b employee = new b("jhon");
 
+        contructor contructor = new contructor("Mike");
+
+        employee.calculateBonus();
+        contructor.work();
+    }
+}
+
+class teammember{
+    protected String name;
+
+    public teammember(String name){
+        this.name = name;
+    }
+}
+
+class b extends teammember{
+    public b(String name){
+        super(name);
+    }
+    public void calculateBonus(){
+        System.out.println(name + " eligible");
+
+    }
+}
+class contructor extends teammember{
+    public contructor (String name){
+        super(name);
+    }
+    public void work(){
+        System.out.println(name + " working");
+    }
+}
