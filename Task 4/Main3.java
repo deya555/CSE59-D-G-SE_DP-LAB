7b2dbf36730889e7bdd22315dfa05db35e69a97a
deya555 <167282964+deya555@users.noreply.github.com>
2026-07-02 17:27:52 +0600

    Implement notification system with email sender

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,29 @@
+public class main43{
+    public static void main(String[] args){
+        MessageSender s = new EmailSender();
 
+        Notification n = new Notification(s);
+
+        n.alertUser("Meeting at 3 PM");
+    }
+}
+
+interface MessageSender{
+    void sendMessage (String message);
+}
+
+class EmailSender implements MessageSender{
+    public void sendMessage(String message){
+        System.out.println("email message: " + message);
+    }
+}
+class Notification{
+    private MessageSender messageSender;
+    public Notification(MessageSender messageSender){
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg){
+        messageSender.sendMessage(msg);
+    }
+}
