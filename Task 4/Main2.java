1fc25f8fc5f2317e512ff1afb6f0b688c08b3289
deya555 <167282964+deya555@users.noreply.github.com>
2026-07-02 17:27:26 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,44 @@
+import java.util.scanner 
+public class main42{
+    public static void main(String[] args){
+        scanner sc = new scanner(System.in);
+        multi copy = new multi();
 
+        copy.printDoc();
+        copy.scanDoc();
+        copy.faxDoc();
+
+        BaseP p = new BaseP();
+        p.printDoc();
+    }
+}
+interface Printer {
+    void printDoc();
+}
+
+interface Scanner {
+    void scanDoc();
+}
+
+interface Fax{
+    void faxDoc();
+}
+class multi implements Printer, Scanner, Fax{
+    public void printDoc(){
+        System.out.println("printing...");
+    }
+
+    public void scanDoc(){
+        System.out.println("Scanning...");
+    }
+
+    public void faxDoc(){
+        System.out.println("Faxing...");
+    }
+}
+
+    class BaseP implements Printer{
+        public void printDoc(){
+            System.out.println("printing...");
+        }
+    }
